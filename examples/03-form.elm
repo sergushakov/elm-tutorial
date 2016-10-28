0843ac3faebf6455462ca132f40014e02e590ced
Sergey Ushakov <sergushakov@yahoo.com>
2016-10-28 22:56:06 +0300

    lesson #2 completed

--- examples/03-form.elm
+++ examples/03-form.elm
@@ -1,11 +1,13 @@
 import Html exposing (..)
-import Html.App as Html
+import Html.App as App
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
+import String exposing (length)
+import Regex
 
-
+main : Program Never
 main =
-  Html.beginnerProgram
+  App.beginnerProgram
     { model = model
     , view = view
     , update = update
@@ -15,28 +17,30 @@ main =
 
 -- MODEL
 
-
 type alias Model =
   { name : String
   , password : String
   , passwordAgain : String
+  , age : Int
+  , valid : Bool
+  , errorMessage : String
   }
 
 
 model : Model
 model =
-  Model "" "" ""
+  Model "" "" "" 0 False ""
 
 
 
 -- UPDATE
 
-
 type Msg
     = Name String
     | Password String
     | PasswordAgain String
-
+    | Age String
+    | Validate
 
 update : Msg -> Model -> Model
 update msg model =
@@ -50,10 +54,19 @@ update msg model =
     PasswordAgain password ->
       { model | passwordAgain = password }
 
+    Age age ->
+      case String.toInt age of
+        Ok newage -> { model | age = newage }
+        Err _ -> { model | age = 0, valid = False, errorMessage = "Age must be a number" } -- TODO: side effect, can be refactored
 
+    Validate ->
+        let
+          (valid, message) = validate model
+        in
+          { model | valid = valid, errorMessage = message }
 
+      
 -- VIEW
-
 
 view : Model -> Html Msg
 view model =
@@ -61,17 +74,31 @@ view model =
     [ input [ type' "text", placeholder "Name", onInput Name ] []
     , input [ type' "password", placeholder "Password", onInput Password ] []
     , input [ type' "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+    , input [ type' "number", placeholder "Age", onInput Age ] []
+    , input [ type' "submit", value "Submit", onClick Validate] []
     , viewValidation model
     ]
 
+validate : Model -> (Bool, String)
+validate model =
+  if model.password /= model.passwordAgain then
+    (False, "Passwords do not match!")
+  else if String.length model.password < 8 then
+    (False, "Password length must be at least 8 chars")
+  else if not (Regex.contains (Regex.regex "[A-Z]") model.password) then
+    (False, "Password length must be at least 1 upcase char")
+  else if not (Regex.contains (Regex.regex "[0-9]") model.password) then
+    (False, "Password length must be at least 1 number")
+  else
+    (True, "")
 
 viewValidation : Model -> Html msg
 viewValidation model =
   let
     (color, message) =
-      if model.password == model.passwordAgain then
+      if model.valid then
         ("green", "OK")
       else
-        ("red", "Passwords do not match!")
+        ("red", model.errorMessage)
   in
-    div [ style [("color", color)] ] [ text message ]
+    div [ style [("color", color),("display", if String.length model.errorMessage > 0 then "block" else "none")] ] [ text message ]
